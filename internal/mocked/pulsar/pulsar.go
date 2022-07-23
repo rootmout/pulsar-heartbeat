69df6ba3ebf32be7340aac3bd54dafc2d546b2bc
rootmout <pierre.kelbert@taurusgroup.ch>
2022-07-23 19:18:00 +0200

    refacto: move payload in a pkg, add mokced pulsar object and add utils

---
+++ internal/mocked/pulsar/pulsar.go
@@ -0,0 +1,6 @@
+package pulsar
+
+//go:generate mockery --name Client --log-level "error" --srcpkg $GOPATH/pkg/mod/github.com/apache/pulsar-client-go@v0.8.1/pulsar --disable-version-string
+//go:generate mockery --name Consumer --log-level "error" --srcpkg $GOPATH/pkg/mod/github.com/apache/pulsar-client-go@v0.8.1/pulsar --disable-version-string
+//go:generate mockery --name Producer --log-level "error" --srcpkg $GOPATH/pkg/mod/github.com/apache/pulsar-client-go@v0.8.1/pulsar --disable-version-string
+//go:generate mockery --name Message --log-level "error" --srcpkg $GOPATH/pkg/mod/github.com/apache/pulsar-client-go@v0.8.1/pulsar --disable-version-string
